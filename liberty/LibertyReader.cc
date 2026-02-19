e232c3ea00ea76fd0d2ca232f37e35f702c20fa4
Stan Lee <135666755+stanminlee@users.noreply.github.com>
2026-02-19 10:06:21 -0800

    can be float or string

--- liberty/LibertyReader.cc
+++ liberty/LibertyReader.cc
@@ -3317,17 +3317,27 @@ LibertyReader::visitShifts(LibertyAttr *attr)
 {
   if (generated_clock_) {
     if (!attr->isComplex()) {
-      libError(1234, attr, "'shifts' attribute is not a complex attribute.");
+      libError(1234, attr, "'shifts' attribute must be a complex attribute.");
     }
     // Initialize edges sequence
     FloatSeq *shifts = new FloatSeq;
     LibertyAttrValueIterator value_iter(attr->values());
     while (value_iter.hasNext()) {
       LibertyAttrValue *value = value_iter.next();
-      float float_value = value->floatValue();
-      shifts->push_back(float_value);
+      // Convert string to float if necessary
+      if (value->isFloat()) {
+        float float_value = value->floatValue();
+        shifts->push_back(float_value);
+      }
+      else if (value->isString()) {
+        const char *string_value = value->stringValue();
+        float float_value = strtof(string_value, nullptr);
+        shifts->push_back(float_value);
+      }
+      else {
+        libError(1234, attr, "shifts attribute must be a float or string.");
+      }
     }
-
     // Error checking, only size 3 is supported at the moment
     if (shifts->size() != 3) {
       delete shifts;
@@ -3346,18 +3356,27 @@ LibertyReader::visitEdges(LibertyAttr *attr)
 {
   if (generated_clock_) {
     if (!attr->isComplex()) {
-      libError(1234, attr, "'edges' attribute is not a complex attribute.");
+      libError(1234, attr, "'edges' attribute must be a complex attribute.");
     }
     // Initialize edges sequence
     IntSeq *edges = new IntSeq;
     LibertyAttrValueIterator value_iter(attr->values());
     while (value_iter.hasNext()) {
       LibertyAttrValue *value = value_iter.next();
-      float float_value = value->floatValue();
-      int int_value = static_cast<int>(float_value);
-      edges->push_back(int_value);
+      // Convert string to float if necessary
+      if (value->isFloat()) {
+        float float_value = value->floatValue();
+        edges->push_back(float_value);
+      }
+      else if (value->isString()) {
+        const char *string_value = value->stringValue();
+        float float_value = strtof(string_value, nullptr);
+        edges->push_back(float_value);
+      }
+      else {
+        libError(1234, attr, "edges attribute must be a float or string.");
+      }
     }
-
     // Error checking, only size 3 is supported at the moment
     if (edges->size() != 3) {
       delete edges;
