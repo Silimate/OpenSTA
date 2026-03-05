e40dde6852479accfc1cd9bffb41d5273544b1d5
Stan Lee <135666755+stanminlee@users.noreply.github.com>
2026-03-05 12:52:24 -0800

    regression but failing

---
+++ test/power_calc_no_inv.tcl
@@ -0,0 +1,19 @@
+# sta_no_inv_power_calc variable should ignore inverter power
+
+# sta_no_inv_power_calc is off
+read_liberty asap7_invbuf.lib.gz
+read_verilog power_calc_no_inv.v
+link_design power_calc_no_inv
+create_clock -name clk -period 1
+set_input_delay -clock clk 0 [all_inputs -no_clocks]
+set_output_delay -clock clk 0 [all_outputs]
+report_power
+
+# sta_no_inv_power_calc is on
+set sta_no_inv_power_calc 1
+read_verilog power_calc_no_inv.v
+link_design power_calc_no_inv
+create_clock -name clk -period 1
+set_input_delay -clock clk 0 [all_inputs -no_clocks]
+set_output_delay -clock clk 0 [all_outputs]
+report_power
