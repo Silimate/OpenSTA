fcdcef564a73023bdac669861f52180bfeea735b
Mohamed Gaber <me@donn.website>
2026-03-17 23:41:53 +0200

    tests: run twice, once with collections and once without
    - tests now run twice, once with collections disabled and once with collections enabled. both compare against the same ok file.

--- test/write_timing_model_scalar.tcl
+++ test/write_timing_model_scalar.tcl
@@ -6,4 +6,8 @@ create_clock -name clk -period 0 clk
 set_input_delay -clock clk 0 [all_inputs -no_clocks]
 set_output_delay -clock clk 0 [all_outputs]
 set_load 1 [all_outputs]
-write_timing_model -scalar results/write_timing_model_scalar.log
+if { $::sta_enable_collections } {
+    write_timing_model -scalar results/write_timing_model_scalar.collections.log
+} else {
+    write_timing_model -scalar results/write_timing_model_scalar.log
+}
