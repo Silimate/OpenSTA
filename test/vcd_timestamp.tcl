7aa7803beda8bb767941a9b2a3a21a7fcd1216ec
Stan Lee <135666755+stanminlee@users.noreply.github.com>
2026-04-12 16:32:13 -0700

    fix

--- test/vcd_timestamp.tcl
+++ test/vcd_timestamp.tcl
@@ -2,16 +2,14 @@
 proc report_activities { } {
   set pins [get_pins -hierarchical *]
   set clk_freq [expr 1.0 / (10 * 1e-12)]
-  puts [format "%-30s %-15s %-15s" "Pin Name" "Activity" "Duty Cycle"]
-  puts [string repeat "-" 60]
+  puts "Pin Name Activity Duty Cycle"
+  puts "--------------------------------------------------------"
   foreach pin $pins {
     set prop [get_property $pin activity]
     set transitions_per_sec [lindex $prop 0]
     set duty [lindex $prop 1]
-    if {[scan $transitions_per_sec "%g" trans_num] == 1 && [scan $duty "%g" duty_num] == 1} {
-      set activity [expr double($trans_num) / [expr $clk_freq * 2]]
-      puts [format "%-30s %-15.6f %-15.6f" [get_full_name $pin] $activity $duty_num]
-    }
+    set activity [expr double($transitions_per_sec) / [expr $clk_freq * 2]]
+    puts "[get_full_name $pin] $activity $duty"
   }
   puts ""
 }
